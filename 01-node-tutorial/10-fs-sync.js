87afd1b439eeaf4a9d5c90b1670204f604acbea0
kapilbhale <62281290+kapilbhale@users.noreply.github.com>
2021-06-20 23:15:52 +0530

    Add files via upload

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -1,15 +1,12 @@
-// const fs = require('fs');
-const { readFileSync, writeFileSync } = require('fs');
-console.log('start');
-const first = readFileSync('./content/first.txt', 'utf-8');
-const second = readFileSync('./content/second.txt', 'utf-8');
-
-// console.log(first, second);
+const { readFileSync, writeFileSync } = require('fs')
+console.log('start')
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
 
 writeFileSync(
-      './content/result-sync.txt',
-      `Here is the result : ${first}, ${second}`,
-      { flag: 'a' }
+  './content/result-sync.txt',
+  `Here is the result : ${first}, ${second}`,
+  { flag: 'a' }
 )
-console.log('done with this task');
-console.log('starting the next one');
+console.log('done with this task')
+console.log('starting the next one')
